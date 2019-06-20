dc4eef1e184ec9515096f31c866c47450bd645c4
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2019-06-20 14:04:59 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,40 @@
+package primesearch;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		int value = Integer.parseInt(input);
+		
+		boolean flag=false;
+		
+		for (int x=2;x<=value;x++)
+		{
+			flag=false;
+			for (int y=2;y<=x/2;y++)
+			{
+				if(x%y==0)
+				{
+					flag=true;					
+				}	
+							
+			}
+			if (flag==false)
+			{
+				System.out.println(x);
+			}
+			
+		}
+		scan.close();
+		
+		
+		
+		
+	}
+
+}
